57769c122cb50e31673b1e2771dfe7caaf3c325e
harryby1149 <harrybeckeryoung@gmail.com>
2021-03-06 19:51:48 -0500

    adding routes for interacting with s3

--- AudioFileService/server.js
+++ AudioFileService/server.js
@@ -1,7 +1,20 @@
+require('dotenv').config()
 const app = require('express')()
 const port = 8001
+const AwsClient = require("./clients/AwsClient");
 
-app.get('/', (req, res) => {res.send('Hello World!')})
+app.get('/audio/:audioObjectId', (req, res) => {
+  AwsClient.getObject(req.params.audioObjectId).then((response)=>{
+        console.log(response.Body);
+        res.send('Hello World!')
+    });
+})
+
+app.post('/audio', (req, res) => {
+    AwsClient.postObject({fileId, body} = req).then((response) =>{
+        console.log(response.Body);
+    })
+})
 
 app.listen(port, () => {
     console.log('app now listening on port: ' + port)
